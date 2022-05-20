4703c9c6b443983e2896f0f4e216146194dd4a7b
Finn Kristian Meland <1390752+fkmeland@users.noreply.github.com>
2022-05-20 07:47:39 +0200

    fix: :bug: fixes an issue with the socket.io and older clients after upgrade

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 // server.js
 
 // set up ======================================================================
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // configuration ===============================================================
 // use public folder for js, css, imgs, etc
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.use(express.static(`${__dirname}/ui-react/build`));
 
 // routes ======================================================================
-require('./app/routes.js')(app);
+require("./app/routes.js")(app);
 
 // launch ======================================================================
 const port = process.env.PORT || 8080;
 
-var theserver = app.listen(port, function() {
-	// call controller functions -------------------------------------------------
-	const io = require('socket.io')(theserver);
+var theserver = app.listen(port, function () {
+  // call controller functions -------------------------------------------------
+  const io = require("socket.io")(theserver, { allowEIO3: true });
 
-	// controller if using room lists
-	var controller = require('./app/socket-controller.js')(io);
+  // controller if using room lists
+  var controller = require("./app/socket-controller.js")(io);
 
-	// log something so we know the server is working correctly
-	console.log(`now we're cooking.`);
+  // log something so we know the server is working correctly
+  console.log(`now we're cooking.`);
 });
